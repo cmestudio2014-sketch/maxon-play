d18ba439fdcdd6d07654d48a91829919284b7685
Maximiliano R. Gama <maximilianor.gama@iMac-de-Maximiliano.local>
2026-09-17 14:08:04 -0300

    Liberar dominio Square Cloud

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-export default defineConfig({ plugins:[react(),tailwindcss(),tsconfigPaths()], build:{outDir:"dist"} });
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+
+  server: {
+    host: "0.0.0.0",
+    port: 80,
+    allowedHosts: ["maxonplay.squareweb.app"],
+  },
+
+  build: {
+    outDir: "dist",
+  },
+});
