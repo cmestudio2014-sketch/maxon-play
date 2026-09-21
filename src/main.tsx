dcd862411f7e347d225c4ded3b8a653714f198e7
cmestudio2014-sketch <cmestudio2014@gmail.com>
2026-09-21 20:30:10 -0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import Home from "./routes/index";
 import Player from "./routes/player";
+import Admin from "./routes/admin";
+import Client from "./routes/cliente";
 import "./styles.css";
 
 const path = window.location.pathname.replace(/\/+$/, "") || "/";
+let App = Home;
+if (path === "/player") App = Player;
+else if (path === "/admin" || path.startsWith("/admin/")) App = Admin;
+else if (path === "/cliente" || path.startsWith("/cliente/")) App = Client;
 
-const App = path === "/player" ? Player : Home;
-
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<React.StrictMode><App /></React.StrictMode>);
